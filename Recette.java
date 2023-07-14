2c519437ac85321e7038e920cd63fffc2b96eac2
Maubin76 <maubin@enssat.fr>
2023-07-14 12:18:15 +0200

    Progress

--- Recette.java
+++ Recette.java
@@ -1,24 +1,75 @@
-import javax.swing.*; // Importe les classes nécessaires pour l'interface graphique Swing
-import java.awt.*; // Importe les classes nécessaires pour la gestion de la fenêtre et des composants graphiques
-import java.awt.event.*; // Importe les classes nécessaires pour la gestion des événements
-import java.io.IOException; // Importe la classe IOException pour la gestion des exceptions liées à l'entrée/sortie
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.jar.Attributes.Name;
 
-public class Recette extends JFrame{
+public class Recette extends JFrame {
+    private JPanel panel;
+    private JButton addButton;
+    private int rowCount; 
 
-    private List<String> listeDynamique = new ArrayList<String>();
+    public Recette() {
+        rowCount = 0;
 
-    public Recette(){
         setTitle("Recette"); // Définit le titre de la fenêtre
         setDefaultCloseOperation(EXIT_ON_CLOSE); // Définit l'action de fermeture de la fenêtre
         setSize(500, 300); // Définit la taille de la fenêtre (largeur, hauteur)
         setResizable(true); // Autorise le redimensionnement de la fenêtre
         setLocationRelativeTo(null); // Centre la fenêtre sur l'écran
 
-        JPanel panel = new JPanel(new GridBagLayout()); // Crée un panneau avec un gestionnaire de disposition GridBagLayout
-        GridBagConstraints constraints = new GridBagConstraints(); // Crée des contraintes pour le placement des composants dans la grille
+        GridBagConstraints constraints = new GridBagConstraints();
+        panel = new JPanel(new GridBagLayout());
+
+        JLabel NomIngredients = new JLabel("Nom");
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weightx = 1.0;
+        constraints.gridwidth = 1;
+        constraints.insets = new Insets(5, 5, 5, 5);
+        panel.add(NomIngredients, constraints);
+
+        JLabel QuantiteIngredients = new JLabel("Quantité");
+        constraints.gridx = 2;
+        panel.add(QuantiteIngredients, constraints);
+
+        addButton = new JButton("+");
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addRow(constraints);
+            }
+        });
+
+        addRow(constraints); // Ajoute la première ligne par défaut
+
+        getContentPane().add(panel, BorderLayout.CENTER);
+        getContentPane().add(addButton, BorderLayout.SOUTH);
+
+        setVisible(true);
     }
 
+    private void addRow(GridBagConstraints constraints) {
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.gridx = 0;
+        constraints.gridy = rowCount;
 
+        JLabel NumberIngredient = new JLabel("Ingrédient " + (rowCount + 1) + " : ");
+        constraints.gridx = 0;
+        constraints.gridy = (rowCount + 1);
+        panel.add(NumberIngredient, constraints);
+
+        JTextField NameIngredientField = new JTextField();
+        constraints.gridx = 1;
+        panel.add(NameIngredientField, constraints);
+
+        JTextField QuantityIngredientField = new JTextField();
+        constraints.gridx = 2;
+        panel.add(QuantityIngredientField, constraints);
+
+        rowCount++;
+
+        revalidate(); // Actualise l'interface graphique pour refléter les changements
+    }
 }
---
+++ Tableau.java
@@ -0,0 +1,9 @@
+public class Tableau {
+    public String case1;
+    public int case2;
+
+    public Tableau (String premireCase, int secondeCase){
+        case1 = premireCase;
+        case2 = secondeCase;
+    }
+}
